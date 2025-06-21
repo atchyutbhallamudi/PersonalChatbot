1f976d411ae4aef6a9a7f7e2020763657499cce1
atchyutbhallamudi <atchyutbhallamudi@gmail.com>
2025-06-21 16:51:37 +0530

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,31 @@
+import os
+import requests
+
+class Chat:
+    def __init__(self):
+        pass
+    
+    def bot(self, prompt):
+        API_URL = "https://router.huggingface.co/novita/v3/openai/chat/completions"
+        headers = {
+            "Authorization": f"Bearer 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",
+        }
+        
+        def query(payload):
+            response = requests.post(API_URL, headers=headers, json=payload)
+            return response.json()
+        
+        response = query({
+            "messages": [
+                {
+                    "role": "user",
+                    "content": prompt
+                }
+            ],
+            "model": "minimaxai/minimax-m1-80k"
+        })
+        
+        rs = response["choices"][0]["message"]["content"]
+        idx = rs.index("</think>")+8
+        rs = rs[idx:]
+        return rs
